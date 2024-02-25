d82f94b8dcb2ab105c12e408c84e6d6d049dd1cc
Yingying Lu <20095278@mail.wit.ie>
2024-02-25 02:22:20 +0000

    Add new movie endpoint.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -54,6 +54,18 @@ export class RestAPIStack extends cdk.Stack {
           },
         }
         );
+
+        const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_16_X,
+          entry: `${__dirname}/../lambdas/addMovie.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: moviesTable.tableName,
+            REGION: "eu-west-1",
+          },
+        });
         
         new custom.AwsCustomResource(this, "moviesddbInitData", {
           onCreate: {
@@ -70,10 +82,11 @@ export class RestAPIStack extends cdk.Stack {
             resources: [moviesTable.tableArn],
           }),
         });
-        
+
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
+        moviesTable.grantReadWriteData(newMovieFn)
 
         // REST API 
     const api = new apig.RestApi(this, "RestAPI", {
@@ -99,6 +112,12 @@ export class RestAPIStack extends cdk.Stack {
     movieEndpoint.addMethod(
       "GET",
       new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
+    );
+
+    // NEW
+    moviesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieFn, { proxy: true })
     );
         
         
